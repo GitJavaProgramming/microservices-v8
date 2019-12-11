599c49ae9156297ee6b07968e28d12bce433bdf3
GitJavaProgramming <455547940@qq.com>
2019-12-11 22:33:08 +0800

    V8版 启动顺序：registry gateway *app UI 别忘启动rabbitmq

---
+++ ui-v8/src/main/java/org/pp/uiv8/UiV8Application.java
@@ -0,0 +1,13 @@
+package org.pp.uiv8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiV8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiV8Application.class, args);
+    }
+
+}
---
+++ ui-v8/src/test/java/org/pp/uiv8/UiV8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pp.uiv8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiV8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
